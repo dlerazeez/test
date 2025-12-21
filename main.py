f2ec4a4216dd2182261d19bdffac75037dc6db54
dlerazeez <dler@laveen-air.com>
2025-12-21 15:59:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
 from app.factory import create_app
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Render / uvicorn should still run with: uvicorn main:app
-app = create_app(base_dir=BASE_DIR)
+app = create_app()
